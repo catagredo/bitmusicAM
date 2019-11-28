61cbe4677fd247656aa313117262763f48d3e310
catagredo <catalina0902@gmail.com>
2019-11-28 12:04:41 -0500

    crearcanciones

---
+++ back/rutas/cancionRutas.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const CancionControl = require('../control/cancionControl');
+
+var api = express.Router(); 
+
+api.post('/registrar-cancion', CancionControl.crearCancion);
+
+module.exports = api ;
